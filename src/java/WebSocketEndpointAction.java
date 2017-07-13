d042cc0f9f8a5c5a19ced64415618e70f998c816
yuji74 <yuji.sogawa@minnie.ai.kyutech.ac.jp>
2017-07-13 15:19:46 +0900

    クライアントが持っているユーザー名をサーバーに送り、サーバで受け取ることができるようにした

--- src/java/WebSocketEndpointAction.java
+++ src/java/WebSocketEndpointAction.java
@@ -8,7 +8,6 @@
  *
  * @author sogawa
  */
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,37 +21,39 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/loadMessage")
 public class WebSocketEndpointAction {
+
     public static List<Session> sessions = new ArrayList<Session>();
 
-	@OnOpen
-	public void onOpen(Session session) {
-		// 開始時
-		sessions.add(session);
-	}
+    @OnOpen
+    public void onOpen(Session session) {
+        // 開始時
+        sessions.add(session);
+    }
 
-	@OnMessage
-	public void onMessage(String message) throws IOException {
-		// クライアントからの受信時
-		for (Session session : sessions) {
-			session.getBasicRemote().sendText("{\"command\":\"message\", \"text\": \"" + message.replace("\\", "\\\\").replace("\"", "\\\"") + "\"}");
-		}
-	}
+    @OnMessage
+    public void onMessage(String message) throws IOException {
+        // クライアントからの受信時
+        String[] messageArray = message.split(":", 2);
+        for (Session session : sessions) {
+            if (messageArray[1].equals("")) {
+                // ユーザー名の取得と登録がここでできる
+            } else {
+                session.getBasicRemote().sendText("{\"command\":\"message\", \"text\": \"" + message.replace("\\", "\\\\").replace("\"", "\\\"") + "\"}");
+            }
+        }
+    }
 
-	@OnError
-	public void onError(Throwable t) throws IOException {
-		// エラー発生時
-		for (Session session : sessions) {
-			session.getBasicRemote().sendText("{\"command\":\"error\", \"text\": \"" + t.getMessage().replace("\\", "\\\\").replace("\"", "\\\"") + "\"}");
-		}
-	}
+    @OnError
+    public void onError(Throwable t) throws IOException {
+        // エラー発生時
+        for (Session session : sessions) {
+            session.getBasicRemote().sendText("{\"command\":\"error\", \"text\": \"" + t.getMessage().replace("\\", "\\\\").replace("\"", "\\\"") + "\"}");
+        }
+    }
 
-        
-        
-        
-        
-	@OnClose
-	public void onClose(Session session) {
-		// 完了時
-		sessions.remove(session);
-	}
+    @OnClose
+    public void onClose(Session session) {
+        // 完了時
+        sessions.remove(session);
+    }
 }
